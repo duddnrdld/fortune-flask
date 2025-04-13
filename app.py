0e5f389b965f9c42ee4e51145e27cd433b2c5486
duddnrdld <duddnrdld1@naver.com>
2025-04-13 10:11:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def result():
     <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <title>오늘의 운세</title>
         <link href="https://fonts.googleapis.com/css2?family=Jua&family=Poor+Story&display=swap" rel="stylesheet">
         <style>
